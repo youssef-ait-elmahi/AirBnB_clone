2fb68387c05b4292e78d2a4e0a76c8ca7f11fb49
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-08-08 18:53:22 +0100

    update number 3

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = obj_dict['created_at'].isoformat()
+        obj_dict['updated_at'] = obj_dict['updated_at'].isoformat()
+        obj_dict['__class__'] = self.__class__.__name__
+        return obj_dict
