8fe5f83477dbb7f19a080578fc0aebc1ef541915
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-09 19:49:08 +0100

    fixing and cleaning up the code

--- models/engines/file_storage.py
+++ models/engines/file_storage.py
@@ -36,6 +36,5 @@ class FileStorage:
                     obj = BaseModel()
                     obj.__dict__.update(obj_dict)
                     FileStorage.__objects[key] = obj
-
         except FileNotFoundError:
             pass
