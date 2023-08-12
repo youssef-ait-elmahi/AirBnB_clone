85d6e64db7baf50cbf2f5ff5fcdc9d9e07907dcb
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-12 18:40:24 +0100

    modifying the test files

--- models/engines/file_storage.py
+++ models/engines/file_storage.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+FileStorage class for storing and retrieving objects.
+"""
+
 import json
 from models.base_model import BaseModel
 import os
 
 
 class FileStorage:
+    """
+    This class represents a file storage system for storing
+    and retrieving objects.
+    """
     __file_path = "file.json"
     __objects = {}
 
