0dc6a497203def910268fe8763de70109289be85
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-12 22:37:09 +0100

    adding : state methode

--- models/state.py
+++ models/state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+The module defines the City class,
+it's also is a subclass of BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+    name = ""
