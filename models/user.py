62e9967fc26797dee33fa908319cf1fac31840be
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-12 17:17:05 +0100

    adding : user code

--- models/user.py
+++ models/user.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines the User class, which inherits from the BaseModel class.
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    This class represents a user and extends the BaseModel class.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
