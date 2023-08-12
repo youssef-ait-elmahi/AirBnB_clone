f935590b8ec0b5b08b176e0fbccb8be9e58fd6e7
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-12 19:37:03 +0100

    addding : amenity methode

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+The module defines the Amenity class,
+it's also is a subclass of BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class for representing amenities.
+
+    Attributes:
+        name (str): Name of amenity.
+    """
+
+    name = ""
