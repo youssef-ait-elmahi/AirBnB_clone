281cae366612c9cfd38e42ef1ee6a24ec678c59c
Ahmedkel <ahmedkeloch@gmail.com>
2023-08-12 19:43:47 +0100

    adding : city methode

--- models/city.py
+++ models/city.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+The module defines the City class,
+it's also is a subclass of BaseModel.
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class for representing cities.
+
+    Attributes:
+        state_id (str): ID of the State.
+        name (str): Name of the city.
+    """
+
+    state_id = ""
+    name = ""
